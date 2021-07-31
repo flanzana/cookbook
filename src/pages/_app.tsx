145d52db3733aae73567fe128d0ce31f6de64da0
zana.flander <zana.flander@kiwi.com>
2021-07-31 19:03:48 +0200

    chore(deps): configure prettier

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
-import 'tailwindcss/tailwind.css'
-import type { AppProps } from 'next/app'
+import "tailwindcss/tailwind.css"
+import type { AppProps } from "next/app"
 
-import '../styles/globals.css'
+import "../styles/globals.css"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,8 @@
 const colors = require("tailwindcss/colors")
 
 module.exports = {
-  mode: 'jit',
-  purge: ['./src/**/*.{js,ts,jsx,tsx}'],
+  mode: "jit",
+  purge: ["./src/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
